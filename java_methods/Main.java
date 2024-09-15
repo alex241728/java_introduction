2161eac24a80bac8f11d25308501639692b8f3b8
alex241728 <alex_241728@163.com>
2024-09-14 20:40:45 -0400

    new push

---
+++ java_methods/Main.java
@@ -0,0 +1,31 @@
+package java_methods;
+
+public class Main {
+    public static void main(String[] args) {
+        /* METHODS */
+
+        String[] que = new String[3];
+        que[0] = "How many States does the USA have?";
+        que[1] = "What is the Capital of the United Kingdom";
+        que[2] = "What is the chemical symbol for Iron";
+
+        String[] ans = new String[3];
+        ans[0] = "50";
+        ans[1] = "London";
+        ans[2] = "Fe";
+
+        outputQuestionsAndAnswers(que, ans);
+        System.out.println(sum(10, 12));
+    }
+
+    public static void outputQuestionsAndAnswers(String[] questions, String[] answers) {
+        for(int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            System.out.println(answers[i]);
+        }
+    }
+
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+}
---
+++ loops/Main.java
@@ -0,0 +1,47 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* LOOPS */
+
+        String[] questions = new String[3];
+        questions[0] = "How many States does the USA have?";
+        questions[1] = "What is the Capital of the United Kingdom";
+        questions[2] = "What is the chemical symbol for Iron";
+
+        String[] answers = new String[3];
+        answers[0] = "50";
+        answers[1] = "London";
+        answers[2] = "Fe";
+
+        for(int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+        for(int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            System.out.println(answers[i]);
+        }
+
+        // foreach
+        for(String question : questions) {
+            System.out.println(question);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        // While loop AND endless!
+        while(true) {
+            System.out.println("Do you want to continue?");
+            if(scanner.next().equals("yes")) {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
